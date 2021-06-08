1ab8acef8ce9147207205c1d9fa6db95ee89dc96
Jakob Heubl <jakob.heubl@sahnee.dev>
2021-06-08 23:56:48 +0200

    Player can now move yay

--- jakobsPlayer.gd
+++ jakobsPlayer.gd
@@ -4,12 +4,26 @@ extends Node2D
 # Declare member variables here. Examples:
 #
 var speed = 150
+var vel = Vector2()
 
+	
 # Called when the node enters the scene tree for the first time.
 func _process(delta):
-  move_local_x(speed*delta)
+	vel = Vector2() #set vel to Vector2(0, 0)
+	if Input.is_key_pressed(KEY_RIGHT): # detecting right key
+		vel.x += 1
+	if Input.is_key_pressed(KEY_LEFT): # detecting right key
+		vel.x -= 1
+	if Input.is_key_pressed(KEY_UP): # detecting right key
+		vel.y -= 1
+	if Input.is_key_pressed(KEY_DOWN): # detecting right key
+		vel.y += 1
+		
+	if vel.length() > 0:
+	   vel = vel.normalized() * speed
 
-
+	position += vel * delta
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass

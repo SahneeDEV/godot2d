5d25e621ccdbc9dae26e78e12897eaa7d951ec28
Jakob Heubl <jakob.heubl@sahnee.dev>
2021-06-09 22:26:06 +0200

    New Player joined the game and zooom

---
+++ TheHero.gd
@@ -0,0 +1,34 @@
+extends KinematicBody2D
+
+var speed = 250
+var vel = Vector2()
+onready var _animated_sprite = $AnimatedSprite
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	vel = Vector2() #set vel to Vector2(0, 0)
+	if Input.is_key_pressed(KEY_RIGHT): # detecting right key
+		_animated_sprite.play("run")
+		vel.x += 1
+	if Input.is_key_pressed(KEY_LEFT): # detecting right key
+		_animated_sprite.play("run")
+		vel.x -= 1
+	if Input.is_key_pressed(KEY_UP): # detecting right key
+		_animated_sprite.play("run")
+		vel.y -= 1
+	if Input.is_key_pressed(KEY_DOWN): # detecting right key
+		_animated_sprite.play("run")
+		vel.y += 1
+		
+	if vel.length() > 0:
+	   vel = vel.normalized() * speed
+	else:
+		_animated_sprite.play("idle")
+
+	position += vel * delta

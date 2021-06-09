a20cdbbc1201b9a5a50ee94463fb8328821fd39f
Jakob Heubl <jakob.heubl@sahnee.dev>
2021-06-09 23:21:06 +0200

    I did things

--- TheHero.gd
+++ TheHero.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 var speed = 250
 var vel = Vector2()
 onready var _animated_sprite = $AnimatedSprite
+export (PackedScene) var Bullet
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,6 +12,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
+	if Input.is_action_pressed("shoot"):
+		_animated_sprite.play("shoot")
 	
 	vel = Vector2() #set vel to Vector2(0, 0)
 	if Input.is_key_pressed(KEY_RIGHT): # detecting right key
@@ -32,3 +36,4 @@ func _process(delta):
 		_animated_sprite.play("idle")
 
 	position += vel * delta
+	
